7bfe6821d9c466631e0688931f90f5ec30629257
necmigunduz <necmigunduz@gmail.com>
2021-03-09 00:22:43 +0300

    Add Insertion Sort 1 Solution

---
+++ insertion_sort_1.rb
@@ -0,0 +1,32 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the insertionSort1 function below.
+def insertionSort1(n, arr)
+    num = arr[arr.size - 1]
+    
+    placed = false
+    (arr.size - 2).downto(0) do |j|
+    if(arr[j] > num)
+      arr[j + 1] = arr[j]
+      p arr
+    else
+      arr[j + 1] = num
+      p arr
+      placed = true
+      break
+    end
+    end
+    if(!placed)
+    arr[0] = num
+    p arr
+end
+end
+
+n = gets.to_i
+
+arr = gets.rstrip.split(' ').map(&:to_i)
+
+insertionSort1 n, arr
