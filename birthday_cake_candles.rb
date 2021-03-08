faa212bcd27851bd73f0f452864409899b0ec001
necmigunduz <necmigunduz@gmail.com>
2021-03-08 23:48:22 +0300

    Add solution to Birthday Cake Candles

---
+++ birthday_cake_candles.rb
@@ -0,0 +1,30 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles)
+    m = candles.max
+    c = candles.count(m)
+
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+candles_count = gets.strip.to_i
+
+candles = gets.rstrip.split.map(&:to_i)
+
+result = birthdayCakeCandles candles
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
