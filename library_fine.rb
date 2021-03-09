b8b9f0e732e10851eaacd0faf9eb522f87b0fa7d
necmigunduz <necmigunduz@gmail.com>
2021-03-09 18:30:20 +0300

    Add Library Fine solution

---
+++ library_fine.rb
@@ -0,0 +1,42 @@
+#!/bin/ruby
+
+require 'json'
+require 'stringio'
+
+# Complete the libraryFine function below.
+def libraryFine(d1, m1, y1, d2, m2, y2)
+    if y2 < y1
+        10000
+    elsif y2 == y1 && m2 < m1
+        500 * (m1 - m2)
+    elsif y2 == y1 && m2 == m1 && d2 < d1
+        15 * (d1 - d2)
+    else 
+        0
+    end
+end
+
+fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+
+d1M1Y1 = gets.rstrip.split
+
+d1 = d1M1Y1[0].to_i
+
+m1 = d1M1Y1[1].to_i
+
+y1 = d1M1Y1[2].to_i
+
+d2M2Y2 = gets.rstrip.split
+
+d2 = d2M2Y2[0].to_i
+
+m2 = d2M2Y2[1].to_i
+
+y2 = d2M2Y2[2].to_i
+
+result = libraryFine d1, m1, y1, d2, m2, y2
+
+fptr.write result
+fptr.write "\n"
+
+fptr.close()
